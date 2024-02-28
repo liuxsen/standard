b6989259f129c3cd95d50d87ede50a415cdec42c
jianhui <liujh1@fosun.com>
2024-02-28 17:59:17 +0800

    check-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 // eslint.config.js
 module.exports = {
   extends: '@antfu',
-  rules: { 'jsonc/sort-keys': 'off' },
+  rules: {
+    'jsonc/sort-keys': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+  },
 }
--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@
 - husky git - 提交拦截
   - 触发lint-staged
   - 触发eslint
+  - 禁止提交冲突的package-lock.json
